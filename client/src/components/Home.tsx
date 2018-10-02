320cdf80e24300164b62cd1e557e0a67f3609bb3
Andres <aguirregzz97@gmail.com>
2018-10-02 16:37:56 -0500

    Initial frontEnd base

---
+++ client/src/components/Home.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import Navbar from './Navbar'
+
+type State = {
+}
+
+type Props = {
+}
+
+export default class Home extends React.Component<Props, State> {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar/>
+            </div>
+        )
+    }
+}
---
+++ client/src/components/routes.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import App from './App'
+
+const routes = [
+  { path: '/', action: () => <App />},
+]
+
+export default routes
